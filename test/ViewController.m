93aca97b4642996c57b4f58ef49b76f191524f2f
970134604 <lyxiaofeng@lyxiaofengdeMacBook-Pro.local>
2017-12-11 16:04:03 +0800

    xx

---
+++ test/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by ly小凤 on 2017/12/11.
+//  Copyright © 2017年 ly小凤. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor =[UIColor redColor];
+}
+
+
+
+
+
+@end
